9e96fb89f04d08372598d5d39102fcb4f6ef3394
Gopalakrishnan Kumar <Gopalakrishnan-Kumar@users.noreply.github.com>
2022-07-09 18:26:40 +0530

    Add files via upload

--- Table_count_demos_deduplicates_2_3.sql
+++ Table_count_demos_deduplicates_2_3.sql
@@ -9,10 +9,13 @@ VALUES (-1),(1),(1),(3),(NULL),(3),(4),(6),(NULL),(8),(9),(10),(1);
 
 SELECT id,val FROM count_demos;
 
-SELECT val FROM count_demos;
+SELECT id,val FROM count_demos;
 
-SELECT val FROM count_demos
+SELECT id,val FROM count_demos
 WHERE sqrt(val) > 2;
 
-SELECT val FROM count_demos
+SELECT id,val FROM count_demos
 WHERE abs(val) < 2;
+
+
+SELECT id FROM count_demos;
