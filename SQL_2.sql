38b9a86821a12f0849276348521342bd0f68d054
Gopalakrishnan Kumar <Gopalakrishnan-Kumar@users.noreply.github.com>
2022-07-13 06:23:34 +0530

    Add files via upload

---
+++ SQL_2.sql
@@ -0,0 +1,7 @@
+SELECT * FROM test.`chassis master base`;
+SELECT  ID FROM `chassis master base`; 
+SELECT CHASSISNO FROM `chassis master base`;
+SELECT MOBILENO FROM `chassis master base`;
+SELECT Contact_Name FROM `chassis master base`;
+SELECT EmailID FROM `chassis master base`;
+SELECT my_rank FROM `chassis master base`;
