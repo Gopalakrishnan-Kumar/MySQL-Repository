57098f0c7aac1f3b1cb319e3c53168fba935c358
Gopalakrishnan Kumar <Gopalakrishnan-Kumar@users.noreply.github.com>
2022-07-23 06:17:03 +0530

    Create us_presidents.sql

---
+++ us_presidents.sql
@@ -0,0 +1,8 @@
+SELECT `us_presidents`.`S.No.`,
+    `us_presidents`.`start`,
+    `us_presidents`.`end`,
+    `us_presidents`.`president`,
+    `us_presidents`.`prior`,
+    `us_presidents`.`party`,
+    `us_presidents`.`vice`
+FROM `test`.`us_presidents`;
